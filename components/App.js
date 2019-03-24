c1b2643393e6fe97730b6c47b6e51b8fef93fedf
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-03-24 08:30:28 +0100

    modified component App

--- components/App.js
+++ components/App.js
@@ -21,6 +21,24 @@ App = React.createClass({
       });
     }.bind(this));
   },
+  getGif: function (searchingText, callback) {  // 1.
+    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+    var xhr = new XMLHttpRequest();  // 3.
+    xhr.open('GET', url);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText).data; // 4.
+        var gif = {  // 5.
+          url: data.fixed_width_downsampled_url,
+          sourceUrl: data.url
+        };
+        callback(gif);  // 6.
+      }
+    };
+    xhr.send();
+  },
+
+
   render: function () {
 
     var styles = {
@@ -33,7 +51,7 @@ App = React.createClass({
       <div style={styles}>
         <h1>Wyszukiwarka GIFow!</h1>
         <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-        <Search />
+        <Search onSearch={this.handleSearch}/>
         <Gif
           loading={this.state.loading}
           url={this.state.gif.url}
